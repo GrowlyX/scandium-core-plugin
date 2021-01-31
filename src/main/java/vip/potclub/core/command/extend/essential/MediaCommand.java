2bbeff8037976534b34dd1df291ddea2f45563eb
GrowlyX <rtaofficialtechie@Gmail.com>
2021-01-30 20:11:44 -0500

    Added external media menu for other player viewing

--- src/main/java/vip/potclub/core/command/extend/essential/MediaCommand.java
+++ src/main/java/vip/potclub/core/command/extend/essential/MediaCommand.java
@@ -1,10 +1,13 @@
 package vip.potclub.core.command.extend.essential;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import vip.potclub.core.command.BaseCommand;
+import vip.potclub.core.menu.extend.media.ExternalMediaMenu;
 import vip.potclub.core.menu.extend.media.MediaMenu;
+import vip.potclub.core.util.Color;
 
 public class MediaCommand extends BaseCommand {
 
@@ -16,7 +19,17 @@ public class MediaCommand extends BaseCommand {
         }
 
         Player player = (Player) sender;
-        new MediaMenu(player).open(player);
+        if (args.length == 0) {
+            new MediaMenu(player).open(player);
+        }
+        if (args.length == 1) {
+            Player target = Bukkit.getPlayerExact(args[0]);
+            if (target != null) {
+                new ExternalMediaMenu(target).open(player);
+            } else {
+                player.sendMessage(Color.translate("&cThat player does not exist."));
+            }
+        }
         return false;
     }
 }
--- src/main/java/vip/potclub/core/command/extend/essential/ProfileCommand.java
+++ src/main/java/vip/potclub/core/command/extend/essential/ProfileCommand.java
@@ -1,6 +1,5 @@
 package vip.potclub.core.command.extend.essential;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
