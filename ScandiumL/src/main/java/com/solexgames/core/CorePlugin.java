ffca689b5bc684cc5338f486c78739dc10a7e5c2
GrowlyX <rtaofficialtechie@Gmail.com>
2021-04-07 21:57:09 -0400

    Added a troll

---
+++ ScandiumL/src/main/java/com/solexgames/core/CorePlugin.java
@@ -0,0 +1,12 @@
+package com.solexgames.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CorePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("sike u thought");
+        System.out.println("it me growlzs haha L");
+    }
+}
