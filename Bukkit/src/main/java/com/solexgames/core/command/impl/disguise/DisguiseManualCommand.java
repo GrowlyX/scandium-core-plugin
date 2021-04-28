87fa2476a7334d7cbb82b7ab7fae42f51417624d
GrowlyX <rtaofficialtechie@Gmail.com>
2021-04-27 22:34:12 -0400

    Small update to DisguiseManualCommand

--- Bukkit/src/main/java/com/solexgames/core/command/impl/disguise/DisguiseManualCommand.java
+++ Bukkit/src/main/java/com/solexgames/core/command/impl/disguise/DisguiseManualCommand.java
@@ -15,6 +15,7 @@ import org.bukkit.entity.Player;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 public class DisguiseManualCommand extends BaseCommand {
 
@@ -60,18 +61,20 @@ public class DisguiseManualCommand extends BaseCommand {
 
                     player.sendMessage(Color.SECONDARY_COLOR + "You've disguised " + Color.MAIN_COLOR + player.getDisplayName() + Color.SECONDARY_COLOR + " as " + Color.translate(rank.getColor() + rank.getItalic()) + disguiseData.getName() + ChatColor.GRAY + " (with a random skin)" + Color.SECONDARY_COLOR + ".");
                 } else {
-                    final UUID uuid = CorePlugin.getInstance().getUuidCache().getUuidFromUsername(args[1]);
+                    CompletableFuture.runAsync(() -> {
+                        final UUID uuid = CorePlugin.getInstance().getUuidCache().getUuidFromUsername(args[1]);
 
-                    if (uuid == null) {
-                        player.sendMessage(ChatColor.RED + "Error: That disguise name is not valid.");
-                        return false;
-                    }
+                        if (uuid == null) {
+                            player.sendMessage(ChatColor.RED + "Error: That disguise name is not valid.");
+                            return;
+                        }
 
-                    final DisguiseData generatedData = CorePlugin.getInstance().getDisguiseManager().getDisguiseData(args[1], uuid);
+                        final DisguiseData generatedData = CorePlugin.getInstance().getDisguiseManager().getDisguiseData(args[1], uuid);
 
-                    CorePlugin.getInstance().getDisguiseManager().disguiseOther(player, generatedData, generatedData, rank);
+                        CorePlugin.getInstance().getDisguiseManager().disguiseOther(player, generatedData, generatedData, rank);
 
-                    player.sendMessage(Color.SECONDARY_COLOR + "You've disguised " + Color.MAIN_COLOR + player.getDisplayName() + Color.SECONDARY_COLOR + " as " + Color.translate(rank.getColor() + rank.getItalic()) + generatedData.getName() + ChatColor.GRAY + " (with a random skin)" + Color.SECONDARY_COLOR + ".");
+                        player.sendMessage(Color.SECONDARY_COLOR + "You've disguised " + Color.MAIN_COLOR + player.getDisplayName() + Color.SECONDARY_COLOR + " as " + Color.translate(rank.getColor() + rank.getItalic()) + generatedData.getName() + ChatColor.GRAY + " (with a random skin)" + Color.SECONDARY_COLOR + ".");
+                    });
                 }
             } else {
                 player.sendMessage(ChatColor.RED + ("Error: That player does not exist."));
