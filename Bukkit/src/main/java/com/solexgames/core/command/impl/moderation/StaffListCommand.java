eee4184db666574e8a0ed2d6e9e50624f82ef585
GrowlyX <rtaofficialtechie@Gmail.com>
2021-05-27 12:55:39 -0400

    Removed some server types
    Signed-off-by: GrowlyX <rtaofficialtechie@Gmail.com>

--- Bukkit/src/main/java/com/solexgames/core/command/impl/moderation/StaffListCommand.java
+++ Bukkit/src/main/java/com/solexgames/core/command/impl/moderation/StaffListCommand.java
@@ -3,12 +3,15 @@ package com.solexgames.core.command.impl.moderation;
 import com.solexgames.core.CorePlugin;
 import com.solexgames.core.command.BaseCommand;
 import com.solexgames.core.command.annotation.Command;
+import com.solexgames.core.player.global.NetworkPlayer;
+import com.solexgames.core.player.ranks.Rank;
 import com.solexgames.core.util.Color;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
@@ -21,21 +24,37 @@ public class StaffListCommand extends BaseCommand {
 
     @Override
     public boolean command(CommandSender sender, String label, String[] args) {
-        sender.sendMessage(ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + StringUtils.repeat("-", 53));
-
-        CorePlugin.getInstance().getRankManager().getSortedRanks().stream()
+        final List<Rank> ranksWithStaffPermissions = CorePlugin.getInstance().getRankManager().getSortedRanks().stream()
                 .filter(rank -> rank.getPermissions().contains("scandium.staff"))
-                .forEach(rank -> {
-                    sender.sendMessage(Color.translate(rank.getColor() + rank.getItalic()) + ChatColor.BOLD.toString() + rank.getName() + ":");
+                .collect(Collectors.toList());
 
-                    CorePlugin.getInstance().getPlayerManager().getAllNetworkProfiles().stream()
-                            .filter(networkPlayer -> networkPlayer.getRankName().equals(rank.getName()))
-                            .map(networkPlayer -> ChatColor.GRAY + " * " + Color.SECONDARY_COLOR + networkPlayer.getName() + ChatColor.GRAY + " (" + DurationFormatUtils.formatDurationWords(System.currentTimeMillis() - networkPlayer.getConnectionTime(), true, true) + ") (" + networkPlayer.getServerName() + ")")
-                            .collect(Collectors.toList())
-                            .forEach(sender::sendMessage);
+        if (ranksWithStaffPermissions.isEmpty()) {
+            sender.sendMessage(ChatColor.RED + "I'm sorry, but there aren't any staff ranks available.");
+            return false;
+        }
 
-                    sender.sendMessage(" ");
-                });
+        sender.sendMessage(ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + StringUtils.repeat("-", 53));
+        sender.sendMessage(Color.MAIN_COLOR + ChatColor.BOLD + "Online Staff:");
+        sender.sendMessage(" ");
+
+        ranksWithStaffPermissions.forEach(rank -> {
+            final List<NetworkPlayer> playersWithSpecifiedRank = CorePlugin.getInstance().getPlayerManager().getAllNetworkProfiles().stream()
+                    .filter(networkPlayer -> networkPlayer.getRankName().equals(rank.getName()))
+                    .collect(Collectors.toList());
+
+            if (playersWithSpecifiedRank.isEmpty()) {
+                return;
+            }
+
+            sender.sendMessage(Color.translate(rank.getColor() + rank.getItalic()) + ChatColor.BOLD.toString() + rank.getName() + ":");
+
+            playersWithSpecifiedRank.stream()
+                    .map(networkPlayer -> ChatColor.GRAY + " * " + Color.SECONDARY_COLOR + networkPlayer.getName() + ChatColor.GRAY + " (" + DurationFormatUtils.formatDurationWords(System.currentTimeMillis() - networkPlayer.getConnectionTime(), true, true) + ") (" + networkPlayer.getServerName() + ")")
+                    .collect(Collectors.toList())
+                    .forEach(sender::sendMessage);
+
+            sender.sendMessage(" ");
+        });
 
         sender.sendMessage(ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + StringUtils.repeat("-", 53));
 
--- Bukkit/src/main/java/com/solexgames/core/enums/ServerType.java
+++ Bukkit/src/main/java/com/solexgames/core/enums/ServerType.java
@@ -8,20 +8,16 @@ import org.bukkit.ChatColor;
 @RequiredArgsConstructor
 public enum ServerType {
 
-    BLARE("Blare", "01", ChatColor.AQUA, ChatColor.DARK_AQUA, "https://dsc.gg/blare", "BlareNetworkRIP", "shop.blare.rip", "blare.rip"),
     SCANDIUM("SolexGames", "04", ChatColor.GOLD, ChatColor.YELLOW, "https://solexgames.com", "SolexDev", "store.solexgames.com", "demo.solexgames.com"),
     METEORITE("Meteorite", "05", ChatColor.DARK_PURPLE, ChatColor.AQUA, "https://discord.com/invite/zydbyNBbX5", "Soon!", "store.meteoritemc.net", "https://discord.com/invite/zydbyNBbX5"),
     ADVENTURE("Adventure", "08", ChatColor.DARK_PURPLE, ChatColor.LIGHT_PURPLE, "https://discord.adventure.rip/", "AdventureRIP", " store.adventure.rip", "adventure.rip"),
-    TERRORPVP("Terror PvP", "09", ChatColor.LIGHT_PURPLE, ChatColor.YELLOW, "https://discord.terrorpvp.rip", "TerrorPvPRip", "store.terrorpvp.rip", "terrorpvp.rip"),
-    MYTHIA("Mythia", "10", ChatColor.DARK_BLUE, ChatColor.GRAY, "https://discord.gg/ykTBmG9KRj", "MythiaMC", "store.mythia.us", "mythia.us"),
+    TERRORPVP("TerrorPvP", "09", ChatColor.LIGHT_PURPLE, ChatColor.YELLOW, "https://discord.terrorpvp.rip", "TerrorPvPRip", "store.terrorpvp.rip", "terrorpvp.rip"),
     VERIS("VerisPvP", "12", ChatColor.DARK_PURPLE, ChatColor.YELLOW, "https://discord.gg/6p4pNbTDxc", "VerisClub", "store.veris.club", "veris.club"),
-    MINEARCADE("MineArcade", "13", ChatColor.DARK_AQUA, ChatColor.YELLOW, "https://dsc.gg/minearcade", "MineArcadeORG", "shop.minearcade.org", "play.minearcade.org"),
     DEVOUT("DevoutPvP", "14", ChatColor.DARK_RED, ChatColor.RED, "discord.gg/devout", "DevoutPvP", "store.devoutpvp.com", "devoutpvp.com"),
     POGPUBLIC("PogPublic", "15", ChatColor.AQUA, ChatColor.GRAY, "https://discord.gg/U4EZ9eh9xd", "Pog_Public", "https://store.pogpublic.com/", "fun.pogpublic.com"),
     APPOLIX("AppolixMC", "16", ChatColor.GOLD, ChatColor.YELLOW, "https://discord.gg/mtrWYcAp7c", "AppolixMC", "https://store.appolixmc.com/", "appolixmc.com"),
     KYRO("Kryo Network", "17", ChatColor.BLUE, ChatColor.WHITE, "https://discord.kyro.rip", "KyroNetwork", "https://store.kyro.rip/", "kyro.rip"),
     PVPBAR("PvPBar", "18", ChatColor.GOLD, ChatColor.YELLOW, "https://pvp.bar/discord", "PvPBarMC", "https://store.pvp.bar", "pvp.bar"),
-    MAGE("Mage Network", "19", ChatColor.DARK_PURPLE, ChatColor.LIGHT_PURPLE, "https://discord.mage.rip", "", "https://store.mage.rip/", "mage.rip"),
     FROSTPVP("FrostPvP", "20", ChatColor.AQUA, ChatColor.WHITE, "https://frostpvp.net/discord", "FrostPvPs", "https://store.frostpvp.net/", "frostpvp.net"),
     AURAPVP("AuraPvP", "21", ChatColor.DARK_RED, ChatColor.GRAY, "https://discord.aurapvp.com", "AuraPvPCOM", "https://store.aurapvp.com/", "aurapvp.com"),
     CURSEDMC("CursedMC", "22", ChatColor.DARK_RED, ChatColor.RED, "https://discord.gg/TVcHvykG", "CursedMCRip", "https://store.cursedmc.rip/", "cursedmc.rip"),
